b567b84014d285ab8dbc17fa79f2fed4650fa66c
Dhatchayini1998 <dhatchayini.1998@gmail.com>
2021-03-25 10:00:49 +0530

    first commit

---
+++ first-maven-app/src/main/java/com/lti/model/Employee.java
@@ -0,0 +1,25 @@
+package com.lti.model;
+
+public class Employee {
+    
+	int empId;
+	String employeeName;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	
+	
+	
+	
+	
+	
+}
